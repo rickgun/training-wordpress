982e9d1d652b8c4388b5c0d21d8902f3c42c3862
rickgun_sie <ricky@softwareseni.com>
2018-05-25 16:09:35 +0700

    Delete unused file navigation menu

--- wp-content/themes/landing-page/navigation-top.php
+++
@@ -1,9 +0,0 @@
-
-<nav id="site-navigation" class="main-navigation" role="navigation">
-
-	<?php wp_nav_menu( array(
-		'theme_location' => 'top',
-		'menu_id'        => 'top-menu',
-	) ); ?>
-
-</nav><!-- #site-navigation -->
